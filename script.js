a6a853caa18ce4b8d479ca58a225bcfdf7834d86
unknown <khansakhan9999@gmail.com>
2025-10-01 21:57:11 +0500

    putting my code on github

---
+++ script.js
@@ -0,0 +1,8 @@
+let name= prompt("please enter your name: ");
+
+document.gotElementByid("welecome")
+textContent = "welcome, " + name;
+
+function goToPage(){
+    window.location.href = "index.html"
+}
